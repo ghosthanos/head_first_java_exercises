470430fe185c8c8a880a7d714c22de30a9c2e18a
Vignesh Kumar <vigneshkumar@Vigneshs-MacBook-Air.local>
2024-08-09 00:18:20 +0530

    first commit

---
+++ src/Chapter11/jukebox/sortmountains/SortMountains.java
@@ -0,0 +1,41 @@
+package Chapter10.sortmountains;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Mountain {
+    String name;
+    int height;
+
+    Mountain(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public String toString() {
+        return name + " " + height;
+    }
+}
+
+public class SortMountains {
+    public static void main(String[] args) {
+        new SortMountains().go();
+    }
+
+    public void go() {
+        List<Mountain> mountains = new ArrayList<>();
+        mountains.add(new Mountain("Longs", 14255));
+        mountains.add(new Mountain("Elbert", 14433));
+        mountains.add(new Mountain("Maroon", 14156));
+        mountains.add(new Mountain("Castle", 14265));
+
+        System.out.println("As entered:\n" + mountains);
+
+
+        mountains.sort((mount1, mount2) -> mount1.name.compareTo(mount2.name));
+        System.out.println("By name:\n" + mountains);
+
+        mountains.sort((mount1, mount2) -> mount2.height - mount1.height);
+        System.out.println("By height:\n" + mountains);
+    }
+}
---
+++ src/Chapter2/MovieTestDrive.java
@@ -0,0 +1,12 @@
+package Chapter2;
+
+class 
+
+
+
+public class Movie {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
